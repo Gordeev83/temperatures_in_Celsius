d7d069ed5b7524168ed2106a9fe81054d513be2b
Gordeev83 <152768178+Gordeev83@users.noreply.github.com>
2024-02-21 09:09:04 +0300

    Добавьте файлы проекта.

---
+++ temperatures_in_Celsius/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace temperatures_in_Celsius
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Выберите действие:");
+            Console.WriteLine("1. Перевести температуру из Фаренгейта в Цельсий");
+            Console.WriteLine("2. Перевести температуру из Цельсия в Фаренгейт");
+
+            int choice = int.Parse(Console.ReadLine());
+
+            if (choice == 1)
+            {
+                Console.Write("Введите температуру в Фаренгейтах: ");
+                double fahrenheit = double.Parse(Console.ReadLine());
+
+                double celsius = (fahrenheit - 32) * 5 / 9;
+                Console.WriteLine("Температура в Цельсиях: " + celsius);
+            }
+            else if (choice == 2)
+            {
+                Console.Write("Введите температуру в Цельсиях: ");
+                double celsius = double.Parse(Console.ReadLine());
+
+                double fahrenheit = (celsius * 9 / 5) + 32;
+                Console.WriteLine("Температура в Фаренгейтах: " + fahrenheit);
+            }
+            else
+            {
+                Console.WriteLine("Неверный выбор!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
